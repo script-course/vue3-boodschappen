320068c96b93340dedbf568350442a840d4fcaaa
Jeroen Postema <jeroen@script.nl>
2021-12-08 17:29:41 +0100

    initial commit, still using vue2 options API syntax

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <GroceriesTable msg="Groceries exercise in Vue 3.0 + Vite" />
+</template>
+
+<script>
+import GroceriesTable from './components/GroceriesTable.vue'
+
+export default {
+  name: 'App',
+  components: {
+    GroceriesTable
+  }
+}
+</script>
